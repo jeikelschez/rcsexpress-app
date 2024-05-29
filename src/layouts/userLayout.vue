174b60501ed5b6d2e102f6ad357fb0555e2ce6bf
jeikel <jeikel.sanchez@quanticvision.com>
2024-05-29 12:12:19 +0000

    Upgrade nom breakpoint

--- src/layouts/userLayout.vue
+++ src/layouts/userLayout.vue
@@ -1,49 +1,51 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <!-- Encabezado de la pagina -->
-    <q-header elevated>
-      <q-toolbar class="bg-indigo-9">
-        <!-- Titulo -->
-        <q-toolbar-title style="font-size: 18px"
-          >Rastreo de Encomiendas</q-toolbar-title
-        >
-        <q-space></q-space>
-        <q-space></q-space>
-        <q-toolbar-title style="font-size: 14px; text-align: right">{{
-          title
-        }}</q-toolbar-title>
-        <q-btn flat dense>
-          <q-avatar size="42px">
-            <img src="~assets/avatar.jpg" />
-          </q-avatar>
-          <q-menu
-            transition-show="flip-right"
-            transition-hide="flip-left"
-            auto-close
+    <q-page-container style="padding: 0px">
+      <!-- Encabezado de la pagina -->
+      <q-header elevated>
+        <q-toolbar class="bg-indigo-9">
+          <!-- Titulo -->
+          <q-toolbar-title style="font-size: 18px"
+            >Rastreo de Encomiendas</q-toolbar-title
           >
-            <q-list style="min-width: 100px">
-              <q-item clickable v-ripple @click="logout">
-                <q-item-section avatar>
-                  <q-icon name="logout" />
-                </q-item-section>
-                <q-item-section>Cerrar Sesión</q-item-section>
-              </q-item>
-            </q-list>
-          </q-menu>
-        </q-btn>
-      </q-toolbar>
-    </q-header>
+          <q-space></q-space>
+          <q-space></q-space>
+          <q-toolbar-title style="font-size: 14px; text-align: right">{{
+            title
+          }}</q-toolbar-title>
+          <q-btn flat dense>
+            <q-avatar size="42px">
+              <img src="~assets/avatar.jpg" />
+            </q-avatar>
+            <q-menu
+              transition-show="flip-right"
+              transition-hide="flip-left"
+              auto-close
+            >
+              <q-list style="min-width: 100px">
+                <q-item clickable v-ripple @click="logout">
+                  <q-item-section avatar>
+                    <q-icon name="logout" />
+                  </q-item-section>
+                  <q-item-section>Cerrar Sesión</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </q-toolbar>
+      </q-header>
 
-    <router-view @change-Title="changeTitle"></router-view>
+      <router-view @change-Title="changeTitle"></router-view>
 
-    <!-- Pie de Pagina -->
-    <q-footer elevated bordered>
-      <div class="text-caption float-right items-center creditos">
-        <p style="font-size: 12px; margin-right: 20px; padding-top: 16px">
-          <strong>© 2022. LOS DERECHOS RESERVADOS. RCS EXPRESS</strong>
-        </p>
-      </div>
-    </q-footer>
+      <!-- Pie de Pagina -->
+      <q-footer elevated bordered>
+        <div class="text-caption float-right items-center creditos">
+          <p style="font-size: 12px; margin-right: 20px; padding-top: 16px">
+            <strong>© 2022. LOS DERECHOS RESERVADOS. RCS EXPRESS</strong>
+          </p>
+        </div>
+      </q-footer>
+    </q-page-container>
   </q-layout>
 </template>
 
