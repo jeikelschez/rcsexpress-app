b7d2d7d2315b638892b6d12f2f2d24b404ad3022
jeikel <jeikel.sanchez@quanticvision.com>
2026-06-19 08:14:52 -0400

    Correccion info chofer en relacion despacho

--- src/pages/operaciones/relacionDespacho.vue
+++ src/pages/operaciones/relacionDespacho.vue
@@ -1761,9 +1761,7 @@ export default {
       factArray.vehiculo = this.selectedUnidad.id
         ? this.selectedUnidad.placas +
           " - " +
-          this.selectedUnidad.descripcion +
-          " - " +
-          this.selectedUnidad.chofer
+          this.selectedUnidad.descripcion
         : "";
       factArray.receptor = this.selectedReceptor.id
         ? this.selectedReceptor
@@ -1842,9 +1840,7 @@ export default {
       factArray.vehiculo = this.selectedUnidad.id
         ? this.selectedUnidad.placas +
           " - " +
-          this.selectedUnidad.descripcion +
-          " - " +
-          this.selectedUnidad.chofer
+          this.selectedUnidad.descripcion
         : "";
       factArray.receptor = this.selectedReceptor.id
         ? this.selectedReceptor
